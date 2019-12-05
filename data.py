4f2cca8c54b8a0eca59a955559f0097af8cf3a3e
panamacheng <toptalent.org@outlook.com>
2019-12-05 13:00:07 -0500

    initialize app

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles = [
+    {
+      'id': 1,
+      'title': 'Article One',
+      'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis doloremque sequi, dolor cupiditate assumenda delectus voluptatum vitae est enim laudantium praesentium necessitatibus quae ex natus consequatur voluptatibus quis esse. Excepturi!',
+      'author': 'David james',
+      'created_date': '04-25-2018' 
+    },
+    {
+      'id': 2,
+      'title': 'Article Two',
+      'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis doloremque sequi, dolor cupiditate assumenda delectus voluptatum vitae est enim laudantium praesentium necessitatibus quae ex natus consequatur voluptatibus quis esse. Excepturi!',
+      'author': 'John Doe',
+      'created_date': '02-25-2018' 
+    },
+    {
+      'id': 3,
+      'title': 'Article Three',
+      'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis doloremque sequi, dolor cupiditate assumenda delectus voluptatum vitae est enim laudantium praesentium necessitatibus quae ex natus consequatur voluptatibus quis esse. Excepturi!',
+      'author': 'Super Panamacheng',
+      'created_date': '07-22-2012' 
+    }
+  ]
+  return articles
